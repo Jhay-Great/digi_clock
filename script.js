d567c18066ab76811b7801a8b8bb03c1d82a1c35
Jhay-Great <alemonai.jenef@gmail.com>
2024-06-21 03:06:01 +0000

    object-oriented clock - implemented the clock object using function constructor

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 // using the date object / creating the date object
 const date = new Date();
-const hour = date.getHours();
-const minute = date.getMinutes();
+const hours = date.getHours();
+const minutes = date.getMinutes();
 const seconds = date.getSeconds();
 
-console.log(date, {hour, minute, seconds})
+console.log(date, {hours, minutes, seconds})
 
 
 // 2. Object-Oriented Clock:
@@ -16,3 +16,23 @@ console.log(date, {hour, minute, seconds})
 // a constructor function for clock creation.
 // o Output Test: Confirm the ability to create clock instances and access
 // their properties.
+
+// creating an object clock alt2.0
+const Clock = function(hours, minutes, seconds) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+}
+const clock = new Clock(hours, minutes, seconds);
+console.log(clock);
+
+/**
+ * Time Formatting:
+• Task: Add methods to the Clock object for formatting time:
+o getFormattedTime(): Returns a string in the format “HH:MM:SS”.
+o get12HourTime(): Returns a string with AM/PM.
+Evaluation:
+o Code Review: Check for accurate time formatting logic within the
+methods.
+o Output Test: Verify that formatted time strings are produced correctly.
+ */
