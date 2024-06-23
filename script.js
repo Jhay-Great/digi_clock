187ac170ac73b709d332583c2345a29080694857
Jhay-Great <alemonai.jenef@gmail.com>
2024-06-23 19:51:41 +0000

    alarm functionality completed & minor changes to stylesheet and markup files

--- script.js
+++ script.js
@@ -15,10 +15,12 @@ const Clock = function(hours, minutes, seconds) {
     this.seconds = seconds;
     this._isTwelveHours = false;
     this._period;
+    this.alarmTimes = [];
+    // this.alarmTimes = {}
 
     this._padInput = (data) => data.toString().padStart(2, 0);
 
-    this.setTimeMode = (isFalse = false) => this._isTwelveHours = isFalse;
+    this.setTimeMode = (is12Hours = false) => this._isTwelveHours = is12Hours;
 
     this.getFormattedTime = function () {
         return `${this._padInput(this.hours)}:${this._padInput(this.minutes)}:${this._padInput(this.seconds)}`;
@@ -48,6 +50,15 @@ const Clock = function(hours, minutes, seconds) {
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
+    }
+
+    // setting alarm
+    this.setAlarm = function (setTime) {
+
+    }
+    // getting alarm
+    this.getAlarm = function () {
+
     }
 }
 
@@ -95,6 +106,27 @@ buttonContainer.addEventListener('click', function(e) {
     
 })
 
+
+// setting alarm listeners
+const alarmTime = document.querySelector('.alarm-clock');
+const alarmButton = document.querySelector('.accept-alarm')
+alarmButton.addEventListener('click', function(e) {
+    // console.log(alarmTime.value);
+    // console.dir(alarmTime);
+
+    const time = alarmTime.value;
+    // console.log(typeof time);
+
+    setAlarm(time);
+    soundAlarm()
+    // console.log(getAlarm());
+    // console.log('alarm going off: ', soundAlarm())
+
+})
+
+
+
+
 // time zone implementation
 const res = (navigator.language)
 console.log(res);
@@ -102,5 +134,62 @@ console.log(res);
 console.log(new Date().toLocaleString(res, {
     timeZone: 'America/New_York'
 }))
+
+const date2 = new Intl.DateTimeFormat(navigator.language);
+console.log(date2)
+
+
+// alarm functionality
+const alarmTimes = [];
+const setAlarm = function (setTime) {
+    if (typeof setTime !== 'string' || !setTime) return;
+
+    const now = new Date();
+    const selectedDate = new Date(setTime);
+
+    if (selectedDate <= now) return 'Cannot select a previous or current time';
+
+    if (alarmTimes.includes(selectedDate)) return 'alarm set time already exists';
+
+    // console.log(setTime);
+
+    const countDown = selectedDate - now;
+
+    alarmTimes.push(countDown);
+    // console.log('alarm set successfully')
+}
+const getAlarm = function () {
+    return alarmTimes;
+}
+const soundAlarm = function () {
+
+    getAlarm().forEach(alarm => {
+        console.log(alarm);
+        setTimeout(() => {
+            console.log('Playing alarm...');
+        }, alarm);
+        
+    });
+
+    console.log('alarm is yet to play')
+    
+    // const now = new Date();
+    // let selectedDate = new Date(alarmTime.value);
+    
+    // if (selectedDate <= now) {
+    //     return 'Cannot select a previous or current time';
+    // }
+
+    // if (alarmTimes.includes(selectedDate)) {
+    //     return 'alarm set time already exists';
+    // }
+    
+    // const countDown = selectedDate - now;
+
+    // setTimeout(() => {
+    //     alert('playing alarm....')
+    // }, countDown);
+    
+}
 
 
